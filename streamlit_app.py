86d832839a98d77f7d5bf0b45af5cc5cdb404ba4
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-04-10 17:26:42 -0700

    💫 Update data of last round

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,21 +21,11 @@ matches, table, stats, player = st.tabs(["Partidos", "Tabla", "Estadísticas", "
 
 with matches:
     st.subheader("Partidos")
-    """
-    """
-    write_a_match(0)
-    """
-    ---
-    """
-    write_a_match(1)
-    """
-    ---
-    """
-    write_a_match(2)
-    """
-    ---
-    """
-    write_a_match(3)
+    for i in reversed(range(7)):
+        write_a_match(i)
+        """
+        ---
+        """
 
 with table:
     st.subheader("Tabla general")
